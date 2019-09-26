3fffcd617f766329602e7f1cd6a9ea3212692fb4
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-26 17:57:51 -0300

    Mostrando los Resultados de la Letra

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import Formulario from './components/Formulario';
 import axios from 'axios';
+import Cancion from './components/Cancion';
 
 function App() {
 	// Utilizar useState con 3 States diferentes
@@ -23,6 +24,15 @@ function App() {
 	return (
 		<Fragment>
 			<Formulario consultarAPILetra={consultarAPILetra} />
+
+			<div className="container mt-5">
+				<div className="row">
+					<div className="col-md-6" />
+					<div className="col-md-6">
+						<Cancion letra={letra} />
+					</div>
+				</div>
+			</div>
 		</Fragment>
 	);
 }
