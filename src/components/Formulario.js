abebbf6538a67208868f03893135c37e8e6086a2
Jorge Barcos <jorgebarcos88@gmail.com>
2019-09-26 17:18:24 -0300

    Leyendo los valores del Formulario

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,6 +1,18 @@
 import React, { useState } from 'react';
 
 function Formulario() {
+	const [ busqueda, agregarBusqueda ] = useState({
+		artista: '',
+		cancion: ''
+	});
+	// Función para actualizar el state de los inputs
+	const actualizarState = (e) => {
+		agregarBusqueda({
+			...busqueda,
+			[e.target.name]: e.target.value
+		});
+	};
+
 	return (
 		<div className="bg-info">
 			<div className="container">
@@ -17,6 +29,7 @@ function Formulario() {
 											className="form-control"
 											name="artista"
 											placeholder="Nombre Artista"
+											onChange={actualizarState}
 											required
 										/>
 									</div>
@@ -29,6 +42,7 @@ function Formulario() {
 											className="form-control"
 											name="cancion"
 											placeholder="Nombre Canción"
+											onChange={actualizarState}
 											required
 										/>
 									</div>
